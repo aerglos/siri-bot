cbe50d53691e0345eaef783a120f194ff6e94545
Chase <72784986+aerglos@users.noreply.github.com>
2020-12-16 19:55:42 -0800

    Update ping.js

--- src/cmds/Bot-Utility/ping.js
+++ src/cmds/Bot-Utility/ping.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: "ping",
     description: "A simple ping command",
+    keywords: ["ping", "utility"],
     execute(message, args) {
-        message.channel.send("Pong!");
+        message.channel.send(`Pong ${message.member}`);
     }
 }
