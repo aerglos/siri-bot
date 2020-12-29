0dc0efc79e30c2e3db52f6d1bd122b8bb7da66dd
Chase <72784986+aerglos@users.noreply.github.com>
2020-12-29 14:38:56 -0800

    e

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ client.on('message', (message) => {
 )
 //End body
 
+client.on('guildCreate', (guild) => {
+    if(guild.id === "750874436928012289") {
+        guild.channels.cache.find(chan => chan.name === "onion").send("Hey! I'm Siri, the multi-purpose bot for Cipollahouse. Currently in development by Chase!")
+    }
+})
+
 
 
 
