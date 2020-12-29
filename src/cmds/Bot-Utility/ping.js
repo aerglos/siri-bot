ae9ab791889b299f05a7260d5b3ff216173056ed
Chase <72784986+aerglos@users.noreply.github.com>
2020-12-29 15:00:22 -0800

    e

--- src/cmds/Bot-Utility/ping.js
+++ src/cmds/Bot-Utility/ping.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js");
 module.exports = {
     name: "ping",
     description: "A simple ping command",
@@ -5,6 +6,11 @@ module.exports = {
     usage: "ping",
     keywords: ["ping", "utility"],
     execute(message, args, commandCollection) {
-        message.channel.send(`Pong ${message.member}`);
+        message.channel.send("Pinging...").then(sent => {
+            let pingEmbed = new Discord.MessageEmbed()
+                .setDescription(`**Heartbeat: ${message.client.ws.ping}ms**\n\n**Ping: ${sent.createdTimestamp - message.createdTimestamp}ms**`)
+                .setColor("GREEN")
+            sent.edit(`${message.author}`, pingEmbed)
+        })
     }
 }
