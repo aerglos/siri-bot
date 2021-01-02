a0265fa0c0f0b5fe2f0f0bec7170291e86c7c659
Chase <72784986+aerglos@users.noreply.github.com>
2021-01-01 20:14:25 -0800

    scrollmenu E

--- src/cmds/Bot-Utility/help.js
+++ src/cmds/Bot-Utility/help.js
@@ -4,7 +4,14 @@ module.exports = {
     description: "Get help on a single command, or all commands",
     args: false,
     usage: `help [command]`,
-    execute(message, args) {
+    async execute(message, args) {
+        function buildCmdEmbed(command) {
+            return new Discord.MessageEmbed()
+                .setColor("RANDOM")
+                .setTitle(command.name)
+                .setDescription(command.description)
+                .addField("Info:", `Requires Arguments: ${command.args}\nUsage: ${command.usage}`)
+        }
         let commandCol = message.client.commandCollection;
         if(!args[0]) {
             let finalMsg = "**COMMAND LIST**\n";
@@ -12,17 +19,39 @@ module.exports = {
                 finalMsg = finalMsg + ` **${v.name}**;\n\`\`\`Description: ${v.description}\nRequires arguments: ${v.args}\nUsage: ${v.usage}\`\`\`\n`
             })
             message.channel.send(finalMsg);
-        } else {
+        } else if(args[0] !== 'scrollMenu') {
             let command = commandCol.get(args[0])
             if(!command) return message.channel.send(`I can't find a command that doesn't exsist!`)
 
-            message.channel.send(
-                new Discord.MessageEmbed()
-                    .setColor("RANDOM")
-                    .setTitle(command.name)
-                    .setDescription(command.description)
-                    .addField("Info:", `Requires Arguments: ${command.args}\nUsage: ${command.usage}`)
-            )
+            message.channel.send(buildCmdEmbed(command))
+        } else if(args[0] === "scrollMenu") {
+            let onCmd = 0;
+            let menuMsg = await message.channel.send(buildCmdEmbed(commandCol.array()[onCmd]));
+
+            menuMsg.react("⬅️")
+            menuMsg.react("➡️")
+
+            let scrollCollector = menuMsg.createReactionCollector((r, u) => {return u.id === message.author.id }, {time: 15000})
+            scrollCollector.on("collect", (r, u) => {
+                switch(r.emoji.name) {
+                    case '⬅️':
+                        if(onCmd > 0) {
+                            onCmd -= 1
+                        }
+                        break;
+                    case '➡️':
+                        if(onCmd < commandCol.array().length) {
+                            onCmd += 1
+                        }
+                        break;
+                    default:
+                        message.channel.send("That's not a valid scroll command.");
+                        break;
+                }
+                menuMsg.edit(buildCmdEmbed(commandCol.array()[onCmd]))
+            })
+
+
         }
     }
 }
