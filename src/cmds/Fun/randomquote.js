70526f4d7a1f58d6044087f35ed6743f86441507
Chase <72784986+aerglos@users.noreply.github.com>
2021-01-01 19:46:30 -0800

    add randomquote

---
+++ src/cmds/Fun/randomquote.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "randomquote",
+    description: "Get a random cipollahouse quote",
+    args: false,
+    usage: 'randomquote [fromuser]',
+    async execute(message, args) {
+        let quoteMsgArray = (await (message.guild.channels.cache.find(c => c.name === "quotes")).messages.fetch({}, true)).array()
+        let selectedQuote = quoteMsgArray[Math.floor(Math.random() * quoteMsgArray.length)]
+
+        message.channel.send(selectedQuote.content)
+    }
+}
