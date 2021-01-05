79c097d9197aa70295e6aa5106118bfb389ee6e1
Chase <72784986+aerglos@users.noreply.github.com>
2021-01-04 16:25:04 -0800

    fix voice

--- src/cmds/Moderator/voice.js
+++ src/cmds/Moderator/voice.js
@@ -8,6 +8,7 @@ module.exports = {
         if(!message.member.roles.cache.has("750894397113106453")) return message.channel.send("You need at least the onioneer role to use this");
         if(!message.member.voice.channel) return message.channel.send("You must be in a channel to use this command");
         let memberChan = message.member.voice.channel;
+        if(!args[0]) return message.channel.send("Please specify a voice command.")
         switch(args[0].toLowerCase()) {
             case 'disconnectall':
                 memberChan.members.forEach(m => {
@@ -44,12 +45,19 @@ module.exports = {
                 memberChan.members.find(m => m.id === message.mentions.users.first().id).voice.setMute(true, message.author.username)
                 message.channel.send("Executed voice command.")
                 break;
+            case 'unmute':
+                if(!message.mentions.members.first() || !message.mentions.members.first().voice.channel) return message.channel.send("You must tag the person to unmute, and they must be in a VC")
+                memberChan.members.find(m => m.id === message.mentions.users.first().id).voice.setMute(false, message.author.username)
+                message.channel.send("Executed voice command.")
+                break;
             case 'tempunmute':
                 if(!message.mentions.members.first() || !message.mentions.members.first().voice.channel) return message.channel.send("You must tag the person to temporarily unmute, and they must be in a VC")
                 if(!args[2]) return message.channel.send("You must specify a time in seconds to mute the user");
                 let memberToTempUnmute = memberChan.members.find(m => m.id === message.mentions.users.first().id)
                 memberToTempUnmute.voice.setMute(false, message.author.username)
+                message.channel.send(`Unmute started on user ${memberToTempUnmute} for ${args[2]} seconds.`)
                 await new Promise(r => setTimeout(r, args[2] * 1000));
+                message.channel.send(`Ended unmute on user ${memberToTempUnmute} after ${args[2]} seconds.`)
                 memberToTempUnmute.voice.setMute(true, message.author.username)
                 break;
             case 'deafen':
@@ -73,7 +81,7 @@ module.exports = {
                 message.channel.send("Moved all members in VC to your VC")
                 break;
             default:
-                return message.channel.send("That's not a voice command");
+                return message.channel.send("That's not a voice command, valid commands are; `disconnectall`, `muteall`, `unmuteall`, `deafall`, `undeafall`, `mute`, `unmute`, `tempunmute`, `deafen`, `disconnect`, and `summon`");
                 break;
         }
     }
